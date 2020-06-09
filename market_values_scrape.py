95a40b5ec39d49f98b55c174783c3efe4cf58d57
eishk <eishk@uw.edu>
2020-06-09 00:59:39 -0700

    flake8 passed
    all of em

--- market_values_scrape.py
+++ market_values_scrape.py
@@ -1,18 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 """
 Eish Kapoor
 CSE 163 AD
 Final Project
 
-This file contains the code that is used to scrape the TransferMarkt database for
-the market values of all the players in the British Premier League at a certain season.
-Using BeautifulSoup, we scraped the necessary sites for each club for a player's name,
-market value and team name to a csv.
+This file contains the code that is used to scrape the TransferMarkt database
+for the market values of all the players in the British Premier League at a
+certain season. Using BeautifulSoup, we scraped the necessary sites for each
+club for a player's name, market value and team name to a csv.
 """
 
 import csv
@@ -23,61 +17,90 @@ import time
 
 def main():
     """
-    In this main function, we pass a list of urls that the methods take and scrape, returning
-    what the information we need. For each season, we just replace the year (2018) with another
-    year (2020) to scrape a different season.
+    In this main function, we pass a list of urls that the methods take and
+    scrape, returning what the information we need. For each season, we just
+    replace the year (2018) with another year (2020) to scrape a different
+    season.
     """
-    first = ['https://www.transfermarkt.us/fc-chelsea/startseite/verein/631/saison_id/2018',
-             'https://www.transfermarkt.us/manchester-united/startseite/verein/985/saison_id/2018',
-             'https://www.transfermarkt.us/fc-arsenal/startseite/verein/11/saison_id/2018',
-             'https://www.transfermarkt.us/manchester-city/startseite/verein/281/saison_id/2018']
+    first = ['https://www.transfermarkt.us/fc-chelsea/startseite/'
+             'verein/631/saison_id/2018',
+             'https://www.transfermarkt.us/manchester-united/startseite/'
+             'verein/985/saison_id/2018',
+             'https://www.transfermarkt.us/fc-arsenal/startseite/'
+             'verein/11/saison_id/2018',
+             'https://www.transfermarkt.us/manchester-city/startseite/'
+             'verein/281/saison_id/2018']
     scraping_function(first)
     time.sleep(10)
-    second = ['https://www.transfermarkt.us/tottenham-hotspur/startseite/verein/148/saison_id/2018',
-              'https://www.transfermarkt.us/fc-liverpool/startseite/verein/31/saison_id/2018',
-              'https://www.transfermarkt.us/fc-everton/startseite/verein/29/saison_id/2018',
-              'https://www.transfermarkt.us/west-ham-united/startseite/verein/379/saison_id/2018']
+    second = ['https://www.transfermarkt.us/tottenham-hotspur/startseite/'
+              'verein/148/saison_id/2018',
+              'https://www.transfermarkt.us/fc-liverpool/startseite/'
+              'verein/31/saison_id/2018',
+              'https://www.transfermarkt.us/fc-everton/startseite/'
+              'verein/29/saison_id/2018',
+              'https://www.transfermarkt.us/west-ham-united/startseite/'
+              'verein/379/saison_id/2018']
     scraping_function(second)
     time.sleep(10)
-    third = ['https://www.transfermarkt.us/leicester-city/startseite/verein/1003/saison_id/2018',
-             'https://www.transfermarkt.us/fc-southampton/startseite/verein/180/saison_id/2018',
-             'https://www.transfermarkt.us/crystal-palace/startseite/verein/873/saison_id/2018',
-             'https://www.transfermarkt.us/stoke-city/startseite/verein/512/saison_id/2018']
+    third = ['https://www.transfermarkt.us/leicester-city/startseite/'
+             'verein/1003/saison_id/2018',
+             'https://www.transfermarkt.us/fc-southampton/startseite/'
+             'verein/180/saison_id/2018',
+             'https://www.transfermarkt.us/crystal-palace/startseite/'
+             'verein/873/saison_id/2018',
+             'https://www.transfermarkt.us/stoke-city/startseite/'
+             'verein/512/saison_id/2018']
     scraping_function(third)
     time.sleep(10)
-    fourth = ['https://www.transfermarkt.us/swansea-city/startseite/verein/2288/saison_id/2018',
-              'https://www.transfermarkt.us/fc-watford/startseite/verein/1010/saison_id/2018',
-              'https://www.transfermarkt.us/newcastle-united/startseite/verein/762/saison_id/2018',
-              'https://www.transfermarkt.us/west-bromwich-albion/startseite/verein/984/saison_id/2018']
+    fourth = ['https://www.transfermarkt.us/swansea-city/startseite'
+              '/verein/2288/saison_id/2018',
+              'https://www.transfermarkt.us/fc-watford/startseite/'
+              'verein/1010/saison_id/2018',
+              'https://www.transfermarkt.us/newcastle-united/startseite/'
+              'verein/762/saison_id/2018',
+              'https://www.transfermarkt.us/west-bromwich-albion/'
+              'startseite/verein/984/saison_id/2018']
     scraping_function(fourth)
     time.sleep(10)
-    fifth = ['https://www.transfermarkt.us/afc-bournemouth/startseite/verein/989/saison_id/2017'
-             'https://www.transfermarkt.us/brighton-amp-hove-albion/startseite/verein/1237/saison_id/2017',
-             'https://www.transfermarkt.us/fc-burnley/startseite/verein/1132/saison_id/2017',
-             'https://www.transfermarkt.us/huddersfield-town/startseite/verein/1110/saison_id/2017']
+    fifth = ['https://www.transfermarkt.us/afc-bournemouth/startseite/'
+             'verein/989/saison_id/2017',
+             'https://www.transfermarkt.us/brighton-amp-hove-albion/'
+             'startseite/verein/1237/saison_id/2017',
+             'https://www.transfermarkt.us/fc-burnley/startseite/'
+             'verein/1132/saison_id/2017',
+             'https://www.transfermarkt.us/huddersfield-town/startseite/'
+             'verein/1110/saison_id/2017']
     scraping_function(fifth)
+
 
 def scraping_function(list_of_urls):
     """
-    Function takes the list of urls and scrapes the urls for the information of every team's
-    player and market value, and then appends into a csv the entry in the format
-    [Name, Value, Team]. Has a sleep timer so the connection isn't rest by TransferMarkt.
-    Changed csv name based on year we were intending to scrape.
+    Function takes the list of urls and scrapes the urls for the information of
+    every team's player and market value, and then appends into a csv the entry
+    in the format [Name, Value, Team]. Has a sleep timer so the connection
+    isn't rest by TransferMarkt. Changed csv name based on year we were
+    intending to scrape.
     """
     file = open('playersvalue_2018.csv', 'a', encoding='utf8')
     writer = csv.writer(file)
     writer.writerow(['Name', 'Value', 'Team'])
     for url in list_of_urls:
-        request = urllib2.Request(url, headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
+        request = urllib2.Request(url,
+                                  headers={'user-agent': 'Mozilla/5.0 '
+                                           '(Macintosh; Intel Mac OS'
+                                           ' X 10_14_6)'
+                                           ' AppleWebKit/537.36 (KHTML,'
+                                           ' like Gecko) Chrome/81.0.'
+                                           '4044.138'
+                                           'Safari/537.36'})
         page = urllib2.urlopen(request)
         soup = BeautifulSoup(page, 'html.parser')
         team_name = soup.find("h1", itemprop="name").find("span").text
-        players = soup.find("table", class_ = "items").find("tbody").find_all("tr", recursive=False)
+        players = soup.find("table", class_="items").find("tbody")
+        players = players.find_all("tr", recursive=False)
         for player in players:
-            name = player.find("a", class_ = "spielprofil_tooltip").text
-            linked = player.find("a", href = True, class_ = "spielprofil_tooltip")
-            link = linked['href']
-            value = player.find("td", class_ = "rechts hauptlink").text
+            name = player.find("a", class_="spielprofil_tooltip").text
+            value = player.find("td", class_="rechts hauptlink").text
             value = value[1:].strip()
             if not value:
                 value = 0
@@ -94,18 +117,6 @@ def scraping_function(list_of_urls):
         time.sleep(5)
     file.close()
 
+
 if __name__ == '__main__':
-    main() 
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+    main()
--- transfers_scrape.py
+++ transfers_scrape.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[31]:
-
-
 """
 Eish Kapoor
 CSE 163 AD
@@ -19,67 +13,82 @@ old team.
 import csv
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
-import time
+
 
 def main():
     """
-    Main method that preprocesses the years and feeds the correct url and file name to the function
-    necessary for creating the csv of soccer transfers for listed years.
+    Main method that preprocesses the years and feeds the correct url and
+    file name to the function necessary for creating the csv of
+    soccer transfers for listed years.
     """
     year = ['2017', '2018', '2020']
-    url = 'https://www.transfermarkt.us/premier-league/transfers/wettbewerb/GB1/plus/?saison_id=2017&s_w=&leihe=0&intern=0&intern=1'
-    url_begin = 'https://www.transfermarkt.us/premier-league/transfers/wettbewerb/GB1/plus/?saison_id='
+    url = 'https://www.transfermarkt.us/premier-league/transfers/wettbewerb/'
+    'GB1/plus/?saison_id=2017&s_w=&leihe=0&intern=0&intern=1'
+    url_begin = 'https://www.transfermarkt.us/premier-league/transfers'
+    '/wettbewerb/GB1/plus/?saison_id='
     url_end = '&s_w=&leihe=0&intern=0&intern=1'
     for y in year:
         url = url_begin + y + url_end
         file_path = 'transfers_' + y + '.csv'
         write_transfer_file(url, file_path)
 
+
 def write_transfer_file(url, file_path):
     """
     Takes given website url and given file_path, and appends to the file
     specified by the file_path rows containing the transfer information for
     every player transferred in and out of the British Premier League in the
-    season specified by the TransferMarkt url. Writes row for each player in the form
-    of ['Name', 'Position', 'Market Value', 'Actual Fee', 'New Team', 'Old Team'].
+    season specified by the TransferMarkt url. Writes row for each player in
+    the form of ['Name', 'Position', 'Market Value', 'Actual Fee', 'New Team',
+    'Old Team'].
     """
     file = open(file_path, 'w', encoding='utf8')
     writer = csv.writer(file)
-    writer.writerow(['Name', 'Position', 'Market Value', 'Actual Fee', 'New Team', 'Old Team'])
-    request = urllib2.Request(url, headers = 
-      {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
+    writer.writerow(['Name', 'Position', 'Market Value', 'Actual Fee',
+                    'New Team', 'Old Team'])
+    request = urllib2.Request(url,
+                              headers={'user-agent': 'Mozilla/5.0 '
+                                       '(Macintosh; Intel Mac OS'
+                                       ' X 10_14_6)'
+                                       ' AppleWebKit/537.36 (KHTML,'
+                                       ' like Gecko) Chrome/81.0.'
+                                       '4044.138'
+                                       'Safari/537.36'})
     page = urllib2.urlopen(request)
     soup = BeautifulSoup(page, 'html.parser')
-    boxes = soup.find("div", class_ = "large-8 columns").find_all("div", class_ = "box", recursive= False)
+    b = soup.find("div", class_="large-8 columns")
+    boxes = b.find_all("div", class_="box", recursive=False)
     for box in boxes:
-        tables = box.find_all("div", class_ = "responsive-table", recursive= False)
+        tables = box.find_all("div", class_="responsive-table",
+                              recursive=False)
         if (tables):
             team = box.find("h2").find("a").text
             in_play = tables[0]
             in_rows = in_play.find("tbody").find_all("tr")
             for row in in_rows:
                 infos = row.find_all("td")
-                name = infos[0].find("a", class_ = "spielprofil_tooltip").text
-                age = infos[2].text
+                name = infos[0].find("a", class_="spielprofil_tooltip").text
                 position = infos[4].text
                 market_value = actual_val(infos[5].text)
                 old_team = infos[7].find('a').text
                 actual_fee = actual_val(infos[8].find('a').text)
                 new_team = team
-                writer.writerow([name, position, market_value, actual_fee, new_team, old_team])
+                writer.writerow([name, position, market_value, actual_fee,
+                                new_team, old_team])
             out_play = tables[1]
             out_rows = out_play.find("tbody").find_all("tr")
             for row in out_rows:
                 infos = row.find_all("td")
-                name = infos[0].find("a", class_ = "spielprofil_tooltip").text
-                age = infos[2].text
+                name = infos[0].find("a", class_="spielprofil_tooltip").text
                 position = infos[4].text
                 market_value = actual_val(infos[5].text)
                 new_team = infos[7].find('a').text
                 actual_fee = actual_val(infos[8].find('a').text)
                 old_team = team
-                writer.writerow([name, position, market_value, actual_fee, new_team, old_team])
+                writer.writerow([name, position, market_value,
+                                actual_fee, new_team, old_team])
     file.close()
+
 
 def actual_val(value):
     """
@@ -107,18 +116,6 @@ def actual_val(value):
             value = value * 1000
     return value
 
+
 if __name__ == '__main__':
-    main() 
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+    main()
