8f7b8ad7dc31753dd8b9a5bddb50e39ffe34ecc6
kunalb123 <bhandarkar.kunal@gmail.com>
2020-06-09 23:01:41 -0700

    cleaning up

--- project_main.py
+++ project_main.py
@@ -1,20 +1,27 @@
 '''
 Authors: Eish Kapoor, Kunal Bhandarkar
 Date: June 1, 2020
-Desc: <change this eventually> this file runs basically all the other
-files and answers all the questions we wanted to answer
+Desc: executes all files to get information on how the financials of the
+Premier League have been affected, how qualitative player descriptions
+relate to monetary value, and analysis on player value due to COVID-19
 '''
 
 from covid19_total_revenue import get_total_revenue
-from covid19_player_value import get_player_value
 from qualitative_player_value import get_qualitative_value
 from q2_data_analysis import main_analysis
 
 
 def main():
+    '''
+    executes all functions imported to answer our three critical questions
+    '''
+    print('\ntotal revenue info:')
     get_total_revenue()
+    print('\ngetting qualitative value numbers:')
     get_qualitative_value()
+    print('\nanalysis on player value')
     main_analysis()
+
 
 if __name__ == '__main__':
     main()
